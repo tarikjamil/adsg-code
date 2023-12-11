b84846d672a4367ce65db0bcca5c0387c0ab9f6d
tarikjamil <tarikjamill@gmail.com>
2023-12-11 17:47:08 +0100

    edits

---
+++ script.js
@@ -0,0 +1,66 @@
+// split type
+let text;
+// Split the text up
+function runSplit() {
+  text = new SplitType("[animation=loading-split]", {
+    types: "lines, words",
+    lineClass: "overflow-hidden",
+    wordClass: "loading-animation-split",
+  });
+}
+
+runSplit();
+
+// Update on window resize
+let windowWidth = $(window).innerWidth();
+window.addEventListener("resize", function () {
+  if (windowWidth !== $(window).innerWidth()) {
+    windowWidth = $(window).innerWidth();
+    text.revert();
+    runSplit();
+  }
+});
+
+gsap.registerPlugin(ScrollTrigger);
+
+// On Page Load
+function pageLoad() {
+  let tl = gsap.timeline();
+
+  tl.to(".main-wrapper", {
+    opacity: 1,
+    ease: CustomEase.create("custom", "M0,0 C0.38,0.005 0.215,1 1,1"),
+    duration: 0.6,
+  });
+
+  // Add a label to mark the starting point of simultaneous animations
+  tl.add("loadingAnimationsStart");
+
+  // Add the 'loading' animation and set its position to the label
+  tl.from(
+    "[animation=loading]",
+    {
+      y: "20rem",
+      opacity: "0",
+      stagger: { each: 0.1, from: "start" },
+      ease: CustomEase.create("custom", "M0,0 C0.38,0.005 0.215,1 1,1"),
+      duration: 0.6,
+    },
+    "loadingAnimationsStart"
+  ); // <-- position parameter set to the label
+
+  // Add the 'loading-reverse' animation and set its position to the label
+  tl.from(
+    "[animation=loading-reverse]",
+    {
+      y: "-20rem",
+      opacity: "0",
+      stagger: { each: 0.1, from: "start" },
+      ease: "Quint.easeOut",
+      duration: 1,
+    },
+    "loadingAnimationsStart"
+  ); // <-- position parameter set to the label
+}
+
+pageLoad();
