782ee6c3fa170285236c79c48a223faf420b8a82
tarikjamil <tarikjamill@gmail.com>
2023-12-13 14:22:23 +0100

    lenis

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+const lenis = new Lenis();
+
+lenis.on("scroll", (e) => {
+  console.log(e);
+});
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
+
 function runSplit() {
   text = new SplitType("[animation=loading-split]", {
     types: "lines",
